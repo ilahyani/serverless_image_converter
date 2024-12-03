7dad7c194933c808ebcef4b8a6ff15cc8bf6ebdf
ilahyani <lahyani799@gmail.com>
2024-12-03 16:54:01 +0100

    [feat] upload functionality done

--- function/src/index.js
+++ function/src/index.js
@@ -3,13 +3,21 @@ const multer = require('multer')
 const { upload_function } = require('./upload_func')
 
 const app = express()
-const upload = multer()
+const upload = multer({ storage: multer.memoryStorage() })
 
 app.get('/', (req, res) => {
     res.json({message: 'function is up'})
 })
 
-app.post ('/upload', upload.single('file'), upload_function)
+app.post ('/upload',
+    upload.fields([{ name: 'image', maxCount: 1 }, { name: 'watermark', maxCount: 1 }]),
+    (req, res, next) => {
+        if (!req.files?.image || !req.files?.watermark) {
+            return res.status(400).json({'error': 'Both image and watermark are required'})
+        }
+        next()
+    },
+    upload_function)
 
 app.listen(process.env.PORT, () => {
     console.log(`stream service running on port ${process.env.PORT}`)
--- function/src/upload_func.js
+++ function/src/upload_func.js
@@ -1,27 +1,63 @@
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner")
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3")
+const sharp = require("sharp");
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION })
+
+const upload_to_s3 = async (bucketName, filename, file) => {
+    uploaded_file = await s3Client.send(
+        new PutObjectCommand({
+            Bucket: bucketName,
+            Key: filename,
+            Body: file,
+            contentType: 'image/jpeg'
+        })
+    )
+    const cmd = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: filename,
+    })
+    const url = await getSignedUrl(s3Client, cmd, { expiresIn: 3600 })
+    return url
+}
 
 exports.upload_function = async (request, response) => {
-    const file = request.file;
-    const s3Client = new S3Client({ region: "us-east-1" });
+    const image = request.files.image[0].buffer
+    const imagename = `${Date.now()}-${request.files.image[0].originalname}`
     try {
-        uploaded_file = await s3Client.send(
-            new PutObjectCommand({
-                Bucket: 'upload-fun',
-                Key: 'test-file',
-                Body: file,
-                contentType: 'image/jpeg'
-            })
-        )
-        const cmd = new GetObjectCommand({
-            Bucket: 'upload-fun',
-            key: 'test-file'
-        })
-        const url = await getSignedUrl(s3Client, cmd, { expiresIn: 3600 })
-        return response.json({uploaded_file: url})
+        og_url = await upload_to_s3(process.env.ORIGINAL_FILES_BUCKET, imagename, image)
     }
     catch (error) {
-        console.error('>>>>>>>>>>> upload failed', error)
-        response.status(503).json({error: 'upload failed'})
+        console.error('original image upload failed', error)
+        return response.status(503).json({error: 'upload failed'})
+    }
+    try {
+        const sfile = sharp(image)
+        const file_md = await sfile.metadata()
+        let watermark = request.files.watermark[0].buffer
+        watermark = await sharp(watermark)
+            .resize({ width: Math.round(file_md.width * 0.2) })
+            .png({ quality: 100 })
+            .toBuffer()
+        watermark = sharp(watermark)
+        const watermark_buffer = await watermark.toBuffer()
+        const watermark_md = await watermark.metadata()
+        const watermarked_file = await sfile.composite([{
+            input: watermark_buffer,
+            left:  Math.floor(file_md.width / 2) - Math.floor(watermark_md.width / 2),
+            top: Math.floor(file_md.height / 2) - Math.floor(watermark_md.height / 2)
+        }]).toBuffer()
+        try {
+            const result_name = `${Date.now()}-edit-${request.files.image[0].originalname}`
+            wm_url = await upload_to_s3(process.env.EDITED_FILES_BUCKET, result_name, watermarked_file)
+        }
+        catch (error) {
+            console.error('original file upload failed', error)
+            return response.status(503).json({error: 'upload failed'})
+        }
+        return response.json({ original_file: og_url, edit_file: wm_url })
+    } catch(error) {
+        console.error('operation failed', error)
+        return response.status(503).json({error: 'upload failed'})
     }
 }
