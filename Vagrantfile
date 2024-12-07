97ee7a639ef4f4d7ed11f658122dbcb3499cb0b8
ilahyani <lahyani799@gmail.com>
2024-12-07 19:02:04 +0100

    dev environment setup

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,10 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-22.04"
-  config.vm.provision "shell", path: "init.sh"
-  config.vm.synced_folder ".", "/home/vagrant/watermark_function"
+  config.vm.provision "shell", path: "init.sh", env: {
+    "AWS_ACCESS_KEY_ID" => ENV['AWS_ACCESS_KEY_ID'],
+    "AWS_SECRET_ACCESS_KEY" => ENV['AWS_SECRET_ACCESS_KEY'],
+    "AWS_REGION" => ENV['AWS_REGION']
+  }
+  config.vm.synced_folder ".", "/home/vagrant/watermark_function", type: "virtualbox"
   config.vm.network "forwarded_port", guest: 3000, host: 3000
 end
--- init.sh
+++ init.sh
@@ -4,14 +4,9 @@ set -e
 trap 'echo "setup Failed. Exiting..."; exit 1;' ERR
 
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y wget unzip
+sudo apt-get install -y wget unzip npm
 
 cd /tmp
-
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 20
 
 sudo wget "https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_amd64.zip"
 sudo unzip "terraform_1.9.8_linux_amd64.zip"
@@ -23,9 +18,19 @@ sudo unzip "awscli-exe-linux-x86_64.zip"
 sudo ./aws/install
 sudo aws --version
 
-# sudo aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-# sudo aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-# sudo aws configure set region "$AWS_REGION"
+sudo -u vagrant aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile vagrant
+sudo -u vagrant aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile vagrant
+sudo -u vagrant aws configure set region "$AWS_REGION" --profile vagrant
+sudo -u vagrant aws configure list --profile vagrant
 
-# cd /home/vagrant/watermark_function/function
-# npm run dev
+sudo wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 18
+
+cd /home/vagrant/watermark_function/function
+npm install -f
+
+cd /home/vagrant/watermark_function/terraform
+terraform init
+sudo -u vagrant terraform apply --auto-approve
