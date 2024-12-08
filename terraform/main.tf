3ea3587085de089b5e512ff6d13744c8749bd2c1
ilahyani <lahyani799@gmail.com>
2024-12-08 16:05:42 +0100

    [feat] terraform state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "tf-backendd"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "ft-state"
+  }
 }
 
 provider "aws" {
